4b9768c40e0ec7a36493796f7682dbd99eee8b89
dongmingjuan <mingjuandong@mingdeMacBook-Pro.local>
2017-09-14 18:20:08 +0800

    implement user can upload and see the picture

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
                   belongs_to :group
                   belongs_to :user
                   validates :content, presence: true
+                  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914031311) do
+ActiveRecord::Schema.define(version: 20170914095447) do
 
   create_table "group_relationships", force: :cascade do |t|
     t.integer  "group_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170914031311) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
