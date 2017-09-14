0c5c5611c24b57b8197b0876e6c729d0bad99358
dongmingjuan <mingjuandong@mingdeMacBook-Pro.local>
2017-09-14 09:58:33 +0800

    implement group crud

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,41 @@
+class GroupsController < ApplicationController
+                  before_action :authenticate_user!
+                  def index
+                   @groups = Group.all
+                  end
+                  def new
+                   @group = Group.new
+                  end
+                  def create
+                    @group = Group.new(group_params)
+                    @group.user = current_user
+                    if @group.save
+                     redirect_to groups_path
+                    else
+                     render :new
+                    end
+                  end
+                  def edit
+                    @group = Group.find(params[:id])
+                  end
+                  def update
+                    @group = Group.find(params[:id])
+                    if @group.update(group_params)
+                     redirect_to groups_path
+                    else
+                     render :edit
+                    end
+                  end
+                  def show
+                    @group = Group.find(params[:id])
+                  end
+                  def destroy
+                    @group = Group.find(params[:id])
+                    @group.destroy
+                    redirect_to groups_path
+                  end
+                  private
+                  def group_params
+                   params.require(:group).permit(:title, :description)
+                  end
+end
