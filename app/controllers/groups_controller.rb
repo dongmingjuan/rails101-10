3199805a1dd63e51eff77f712d751b61debededa
dongmingjuan <mingjuandong@mingdeMacBook-Pro.local>
2017-09-14 10:49:09 +0800

    implement post crud

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,41 +1,42 @@
 class GroupsController < ApplicationController
                   before_action :authenticate_user!
                   def index
-                   @groups = Group.all
+                    @groups = Group.all
+                  end
+                  def show
+                    @group = Group.find(params[:id])
+                    @posts = @group.posts
                   end
                   def new
-                   @group = Group.new
+                    @group = Group.new
                   end
                   def create
                     @group = Group.new(group_params)
                     @group.user = current_user
                     if @group.save
-                     redirect_to groups_path
+                      redirect_to groups_path
                     else
-                     render :new
+                      render :new
                     end
                   end
                   def edit
-                    @group = Group.find(params[:id])
+                     @group = Group.find(params[:id])
                   end
                   def update
-                    @group = Group.find(params[:id])
-                    if @group.update(group_params)
-                     redirect_to groups_path
-                    else
-                     render :edit
-                    end
-                  end
-                  def show
-                    @group = Group.find(params[:id])
+                     @group = Group.find(params[:id])
+                     if @group.update(group_params[:id])
+                        redirect_to groups_path
+                     else
+                        render :edit
+                     end
                   end
                   def destroy
-                    @group = Group.find(params[:id])
-                    @group.destroy
-                    redirect_to groups_path
+                     @group = Group.find(params[:id])
+                     @group.destroy
+                     redirect_to groups_path
                   end
                   private
                   def group_params
-                   params.require(:group).permit(:title, :description)
+                    params.require(:group).permit(:title, :description)
                   end
 end
